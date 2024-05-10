29be5d6a9f1e3814ab9415dc5168bc3402e278de
atrainag <jerichogan8@gmail.com>
2024-05-11 00:53:01 +0800

    added some lsp and auto reload files

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 require("atrainag.core")
 require("atrainag.lazy")
+
+-- Auto-read when a file is changed from the outside
+vim.o.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    vim.cmd("checktime")
+  end,
+})
--- lua/atrainag/plugins/lsp/lspconfig.lua
+++ lua/atrainag/plugins/lsp/lspconfig.lua
@@ -108,7 +108,7 @@ return {
         -- configure emmet language server
         lspconfig["emmet_ls"].setup({
           capabilities = capabilities,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          filetypes = { "vue", "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
         })
       end,
       ["lua_ls"] = function()
