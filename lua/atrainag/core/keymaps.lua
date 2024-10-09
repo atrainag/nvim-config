98d97788e47c3be02b5fa875aa93c762d4b7d62d
atrainag <jerichogan8@gmail.com>
2024-10-09 15:54:37 +0800

    Added multiple lines edit for I and A

--- lua/atrainag/core/keymaps.lua
+++ lua/atrainag/core/keymaps.lua
@@ -29,3 +29,22 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<S-tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>te", "<cmd>tabedit<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Utilities
+keymap.set("x", "<S-A>", "v:lua.check_visual_mode_append()", { noremap = true, expr = true })
+function _G.check_visual_mode_append()
+  if vim.fn.mode() == "V" then
+    return "<C-v>$A"
+  else
+    return "A"
+  end
+end
+
+keymap.set("x", "<S-I>", "v:lua.check_visual_mode_insert()", { noremap = true, expr = true })
+function _G.check_visual_mode_insert()
+  if vim.fn.mode() == "V" then
+    return "<C-v>$I"
+  else
+    return "I"
+  end
+end
